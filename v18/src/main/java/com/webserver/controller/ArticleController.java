13f2fd01c732651b3bff43b5860cad79fbd00180
彭靖桐 <7494268+TonyPeng123@user.noreply.gitee.com>
2022-04-14 16:41:23 +0800

    v18

---
+++ v18/src/main/java/com/webserver/controller/ArticleController.java
@@ -0,0 +1,11 @@
+package com.webserver.controller;
+
+public class ArticleController {
+    /**
+     * 处理发表文章的方法
+     */
+    public void writeArticle(){
+
+    }
+
+}
---
+++ v18/src/main/java/com/webserver/entity/Article.java
@@ -0,0 +1,4 @@
+package com.webserver.entity;
+
+public class Article {
+}
